78ce228b4b96fee9dc2c77b89605cacfb9c4f700
bmoran@enertia-inc.com <brian.moran@dualbootpartners.com>
2020-08-01 16:51:11 -0400

    Initial Group Work React App.

---
+++ group-work/AlphaTeamTwo/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Brian Moran		 	
+		</p>
+		<p>
+		   Chris Bush
+		</p>
+		<p>
+			David Akinbode
+		</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
